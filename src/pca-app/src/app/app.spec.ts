9a10f07901fba3f7f5ffbcbb27adf93d7ce5fbb0
Stefan Minchev <sminchev@outlook.com>
2026-08-16 14:49:58 +0200

    #3 feat: primeng, proxy, env setups + cleanup default page

--- src/pca-app/src/app/app.spec.ts
+++ src/pca-app/src/app/app.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter([]), MessageService],
     }).compileComponents();
   });
 
@@ -12,12 +15,5 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pca-app');
   });
 });
